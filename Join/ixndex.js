2819a2d4e93522d65b36a848dbc85f6ff24b0590
Kapatok <Kapatochik@gmail.com>
2021-04-06 18:27:00 +0300

    dop
    +

---
+++ Join/ixndex.js
@@ -0,0 +1,154 @@
+//доп 1 **
+//=====================================================================//
+
+// const num1 = +prompt('enter a number');
+// const num2 = +prompt('enter a number');
+
+// function sr(num1, num2) {
+//     if (num1 >= num2) {
+//         console.log(true);
+//     } else {
+//         if (num1 <= num2) {
+//             console.log(false);
+//         }
+//     }
+// }
+// sr(num1, num2);
+
+
+
+
+//доп 2** 
+//===================================================================//
+
+// const num1 = +prompt('enter a number');
+// const num2 = +prompt('enter a number');
+// const num3 = 10;
+
+// function sr(num1, num2) {
+//     if (num3 >= num1 + num2) {
+//         console.log(true);
+//     } else {
+//         if (num3 <= num1 <= num2) {
+//             console.log(false);
+//         }
+//     }
+// }
+
+// sr(num1, num2, num3);
+
+
+
+
+
+//до3**
+//====================================================================//
+
+// var ven = +prompt('enter a number')
+
+// function ers(ven) {
+//     if (ven % 2 == 0) {
+//         console.log(true);
+//     } else {
+//         if (ven % 1 == 0) {
+//             console.log(false);
+//         }
+//     }
+// }
+// ers(ven);
+
+
+//4**
+//====================================================================//
+
+// const arr = ['orange', 'banana', 'pear'];
+// console.log(arr);
+
+// const arr2 = new Array(3);
+// console.log(arr2.length);
+// arr.push('apple', 'a pineapple');
+// console.log(arr);
+// arr.unshift('grapefruit');
+// console.log(arr);
+// const arr3 = new Array(6);
+// console.log(arr3.length);
+// arr.pop();
+// arr.shift();
+// console.log(arr);
+// const arr4 = new Array(4);
+// console.log(arr4.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//5**
+//====================================================================//
+
+
+
+// function sumSquares(min, max) {
+//     if (!Number.isInteger(min) || !Number.isInteger(max) || max < min);
+//     let sum = 0;
+//     for (let i = min; i <= max; i++) sum += i * i;
+//     return sum;
+// }
+
+// console.log(sumSquares(1, 6));
+
+
+
+//6**
+//====================================================================//
+
+// const num = sum(24, 2);
+// const num1 = sum(5, 65);
+
+// function sum(num, num1) {
+//     return num + num1;
+// }
+// console.log(num);
+// console.log(num1);
+
+
+
+
+
+//до 7 **
+//====================================================================//
+
+// let weekday = +prompt('weekday 1-7');
+
+// switch (weekday) {
+//     case 1:
+//         console.log('Monday');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;
+//     case 4:
+//         console.log('Thursday');
+//         break;
+//     case 5:
+//         console.log('Friday');
+//         break;
+//     case 6:
+//         console.log('Saturday');
+//         break;
+//     case 7:
+//         console.log('Saturday');
+//     default:
+//         break;
+// }
+//====================================================================//
